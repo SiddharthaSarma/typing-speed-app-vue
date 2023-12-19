5cf13563a425c7d1e9aa2b9e6a1833340b9bc74e
siddharthasarma <sidduinevitable@gmail.com>
2023-12-19 18:54:46 +0530

    update text generator component

--- src/components/TextGenerator.vue
+++ src/components/TextGenerator.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+const text = ref(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia necessitatibus culpa alias, quis quae assumenda maxime
+    blanditiis neque beatae unde deserunt sunt quas, vel aliquam illum expedita perspiciatis odio minus?`)
+const textSplit = text.value.split(' ')
 
 </script>
 <template>
-  <h2>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia necessitatibus culpa alias, quis quae assumenda maxime
-    blanditiis neque beatae unde deserunt sunt quas, vel aliquam illum expedita perspiciatis odio minus?
-  </h2>
+  <div :class="$style.content">
+    <span :class="$style.word" v-for="(t, i) in textSplit" :key="i">{{ t }}</span>
+  </div>
 </template>
+<style module>
+.content {
+  font-size: 26px;
+}
+
+.word {
+  margin: 0px 3px;
+}
+</style>
