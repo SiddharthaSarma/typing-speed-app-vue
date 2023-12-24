ba0e921798154368e8281ada708f780567d43f64
siddharthasarma <sidduinevitable@gmail.com>
2023-12-24 18:25:01 +0530

    store user typed words

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@ import TextGenerator from './components/TextGenerator.vue';
 import { ref } from 'vue';
 
 const typingText = ref('');
+const typedWords = ref<string[]>([])
 const showTimer = ref(false);
 const text = ref(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia necessitatibus culpa alias, quis quae assumenda maxime blanditiis neque beatae unde deserunt sunt quas, vel aliquam illum expedita perspiciatis odio minus?`);
 
 const handleUserTyping = (input: Event) => {
   const inputValue = (input as InputEvent).data;
   if (inputValue === ' ') {
+    if (typingText) {
+      typedWords.value.push(typingText.value)
+    }
     typingText.value = ''
   }
 };
@@ -18,6 +22,7 @@ const startTyping = () => {
 };
 const onTimerComplete = () => {
   showTimer.value = false;
+  console.log(typedWords.value)
 };
 </script>
 
@@ -25,8 +30,8 @@ const onTimerComplete = () => {
   <h1>Typing speed</h1>
   <Timer v-if="showTimer" :timer="30" @timer-complete="onTimerComplete" />
   <button v-else @click="startTyping">Start Typing</button>
-  <TextGenerator :text="text"/>
-  <input v-if="showTimer" v-model="typingText" @input="handleUserTyping" class="typing-input"/>
+  <TextGenerator :text="text" />
+  <input v-if="showTimer" v-model="typingText" @input="handleUserTyping" class="typing-input" />
 </template>
 
 <style scoped>
@@ -36,5 +41,4 @@ const onTimerComplete = () => {
   margin-top: 2rem;
   font-size: 2rem;
 }
-
 </style>
